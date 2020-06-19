b83b6c817f438d6eeaac1cb4c1820ff087e036cd
Neha218 <nehaapawar21@gmail.com>
2020-06-19 19:05:54 +0530

    Add to cart

--- routes/cart.js
+++ routes/cart.js
@@ -1,15 +1,47 @@
 const express = require("express");
 const router = express.Router();
+
+// Get product model
+var Product = require("../models/product");
+
 /*
- *  Get /
+ *  Get add product to cart
  */
-router.get("/", (req, res) => {
-  Page.findOne({ slug: "home" }, (err, page) => {
+router.get("/add/:product", (req, res) => {
+  var slug = req.params.product;
+  Product.findOne({ slug: slug }, (err, product) => {
     if (err) console.log(err);
-    res.render("index", {
-      title: page.title,
-      content: page.content
-    });
+    if (typeof req.session.cart == "undefined") {
+      req.session.cart = [];
+      req.session.cart.push({
+        title: slug,
+        qty: 1,
+        price: parseFloat(product.price).toFixed(2),
+        image: product.image
+      });
+    } else {
+      var cart = req.session.cart;
+      var newItem = true;
+
+      for (i = 0; i < cart.length; i++) {
+        if (cart[i].title == slug) {
+          cart[i].qty++;
+          newItem = false;
+          break;
+        }
+      }
+
+      if (newItem) {
+        cart.push({
+          title: slug,
+          qty: 1,
+          price: parseFloat(product.price).toFixed(2),
+          image: product.image
+        });
+      }
+    }
+    req.flash("success", "Product added to the cart!");
+    res.redirect("back");
   });
 });
 
