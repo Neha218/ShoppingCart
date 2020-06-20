ae9403253d005d4ff16f9f6f242b54919b5849bf
Neha218 <nehaapawar21@gmail.com>
2020-06-20 23:08:08 +0530

    Register Post

--- routes/users.js
+++ routes/users.js
@@ -16,20 +16,58 @@ router.get("/register", (req, res) => {
 });
 
 /*
- *  Get a page
+ * Post register
  */
-router.get("/:slug", (req, res) => {
-  var slug = req.params.slug;
-  Page.findOne({ slug: slug }, (err, page) => {
-    if (err) console.log(err);
-    if (!page) res.redirect("/");
-    else {
-      res.render("index", {
-        title: page.title,
-        content: page.content
-      });
-    }
-  });
+router.post("/register", (req, res) => {
+  var name = req.body.name;
+  var email = req.body.email;
+  var username = req.body.username;
+  var password = req.body.password;
+  var password2 = req.body.password2;
+
+  req.checkBody("name", "Name is required!").notEmpty();
+  req.checkBody("email", "Please enter valid email id!").isEmail();
+  req.checkBody("username", "Username is required!").notEmpty();
+  req.checkBody("password", "Password is required!").notEmpty();
+  req.checkBody("password2", "Password do not match!").equals(password);
+
+  var errors = req.validationErrors();
+
+  if (errors) {
+    res.render("register", {
+      errors,
+      title: "Register"
+    });
+  } else {
+    User.findOne({ username: username }, (err, user) => {
+      if (err) console.log(err);
+      if (user) {
+        req.flash("danger", "Username exists, choose another!");
+        res.redirect("/users/register");
+      } else {
+        var user = new User({
+          name,
+          email,
+          username,
+          password,
+          admin: 0
+        });
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(user.password, salt, (err, hash) => {
+            if (err) console.log(err);
+            user.password = hash;
+            user.save(err => {
+              if (err) console.log(err);
+              else {
+                req.flash("success", "You are now registered!");
+                res.redirect("/users/login");
+              }
+            });
+          });
+        });
+      }
+    });
+  }
 });
 
 // Exports
